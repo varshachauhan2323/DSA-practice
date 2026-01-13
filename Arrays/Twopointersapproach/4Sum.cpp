08be9814038528cb87322265917bd5b1bc07d66a
Varsha <chauhanvarsha2323@gmail.com>
2026-01-13 11:02:30 +0530

    Added 4Sum problem

---
+++ Arrays/Twopointersapproach/4Sum.cpp
@@ -0,0 +1,36 @@
+//Platform:Leetcode
+// Sort array
+// Fix two elements, use two pointers for remaining sum
+// Skip duplicates at all levels
+// Time: O(n^3)
+// Space: O(1) extra space (excluding output)
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(i>0&&nums[i]==nums[i-1])continue;
+            for(int j=i+1;j<nums.size();j++){
+                            if(j!=i+1&&nums[j]==nums[j-1])continue;
+
+                int p=j+1;
+                int q=nums.size()-1;
+                while(p<q){
+                    long long sum=(long long)nums[i]+(long long)nums[j]+(long long)nums[p]+(long long)nums[q];
+                    if(sum>target) q--;
+                    else if(sum<target) p++;
+                    else{
+                        ans.push_back({nums[i],nums[j],nums[p],nums[q]});
+                        p++;
+                        q--;
+                        while(p<q&&nums[p]==nums[p-1]) p++;
+                    }
+                }
+
+            }
+        }
+        return ans;
+    }
+};
