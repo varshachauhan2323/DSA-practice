f00e866b8862de3ef60cf708d866e96297643703
Varsha <chauhanvarsha2323@gmail.com>
2026-01-20 17:12:02 +0530

    Added binary search problem

---
+++ Arrays/Binary_search/searchinsertpos.cpp
@@ -0,0 +1,27 @@
+
+/*
+ Search Insert Position
+Platform: LeetCode
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = nums.size();
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
