3d52012c82ca2258ff653b593d80b1d89278692f
Varsha <chauhanvarsha2323@gmail.com>
2026-01-22 11:14:59 +0530

    Added binary search problem

---
+++ Arrays/Binary_search/search_in_rowwise.cpp
@@ -0,0 +1,20 @@
+/*
+ Search in Row and Column Wise Sorted Matrix
+Platform: LeetCode
+Time Complexity: O(n + m)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int i = 0, j = matrix[0].size() - 1;
+
+        while (i < matrix.size() && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) j--;
+            else i++;
+        }
+        return false;
+    }
+};
