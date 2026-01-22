a4c81b8f9c98e0bba3c1a56c3ea76cea3d5ebd48
Varsha <chauhanvarsha2323@gmail.com>
2026-01-22 11:17:49 +0530

    Added string  problem

---
+++ Strings/largest_odd_no.cpp
@@ -0,0 +1,17 @@
+/*
+Question Name: Largest Odd Number in a String
+Platform: LeetCode
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2 == 1)
+                return num.substr(0, i + 1);
+        }
+        return "";
+    }
+};
