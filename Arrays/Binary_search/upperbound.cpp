24f8ac3b36b28089e85fd1b2f46944f468247d0e
Varsha <chauhanvarsha2323@gmail.com>
2026-01-22 11:15:38 +0530

    Added binary search problem

---
+++ Arrays/Binary_search/upperbound.cpp
@@ -0,0 +1,24 @@
+/*
+ Implement Upper Bound
+Platform: LeetCode
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int upperBound(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        int ans = nums.size();
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > target) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
