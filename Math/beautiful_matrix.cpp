cdf85f63150106275304fcfd1cd47da6cbb33204
Varsha <chauhanvarsha2323@gmail.com>
2026-02-11 20:09:39 +0530

    Add Math  problem

---
+++ Math/beautiful_matrix.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int x;
+    int row,col;
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            cin>>x;
+            if(x==1){
+                row=i;
+                col=j;
+            }
+        }
+    }
+    cout<<abs(row-3)+abs(col-3);
+    return 0;
+}
