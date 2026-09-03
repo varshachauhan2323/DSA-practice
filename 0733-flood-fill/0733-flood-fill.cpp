0148b87fb595c190cd4bb142599aa26659a80fa0
varshachauhan2323 <chauhanvarsha2323@gmail.com>
2026-09-03 23:02:07 +0530

    Time: 0 ms (100%), Space: 18.5 MB (19.68%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&image,int sr,int sc,int color,int inicolor,vector<vector<int>>&ans,vector<vector<int>>&vis){
-        ans[sr][sc]=color;
-        vis[sr][sc]=1;
-        int n=image.size();
-        int m=image[0].size();
-        int delrow[]={-1,0,1,0};
-        int delcol[]={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int newrow=sr+delrow[i];
-            int newcol=sc+delcol[i];
-            if(newrow>=0&&newrow<n&&newcol>=0&&newcol<m&&!vis[newrow][newcol]&&image[newrow][newcol]==inicolor){
-                dfs(image,newrow,newcol,color,inicolor,ans,vis);
-            }
+void dfs(vector<vector<int>>&image,int sr,int sc,int color,vector<vector<int>>&vis,int oc,int n,int m,    vector<vector<int>>&ans){
+    vis[sr][sc]=1;
+    ans[sr][sc]=color;
+    int dr[]={-1,0,1,0};
+    int dc[]={0,1,0,-1};
+    for(int i=0;i<4;i++){
+        int newr=sr+dr[i];
+        int newc=sc+dc[i];
+        if(newr>=0&&newr<n&&newc>=0&&newc<m&&!vis[newr][newc]&&image[newr][newc]==oc){
+                dfs(image,newr,newc,color,vis,oc,n,m,ans);
+            
         }
     }
+}
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int inicolor=image[sr][sc];
-        vector<vector<int>>ans=image;
         int n=image.size();
         int m=image[0].size();
+        vector<vector<int>>ans=image;
         vector<vector<int>>vis(n,vector<int>(m,0));
-        dfs(image,sr,sc,color,inicolor,ans,vis);
+        int oc=image[sr][sc];
+        dfs(image,sr,sc,color,vis,oc,n,m,ans);
         return ans;
     }
 };
